e33c075665835d98536255ec6673892b559228dd
Madhu Patel <54492585+madhup01@users.noreply.github.com>
2021-01-23 22:45:14 +0530

    Create error404.jsx

---
+++ client/src/error404.jsx
@@ -0,0 +1,23 @@
+
+//Add route for error page...
+// <Route component = {error}>
+import React from "react";
+import logo from './design/error-screen/sreens/error-sreen-desktop.png'
+const Error = () => {
+    return (
+        <>
+        <div className="setStyle2">
+            <h1>404 Error Page</h1>
+            <p>Sorry this page is not valid</p>
+            <NavLink to ="/">Take Me Back To Where I Came From</NavLink>
+        
+        </div>
+        </>
+    );
+};
+console.log(error-sreen-desktop);
+function Header() {
+  
+  return <img src={error-sreen-desktop} alt="Logo" />;
+}
+export default Header;
