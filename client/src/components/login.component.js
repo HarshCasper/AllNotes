30b088c49d0e0a43b2236c0ef3e9359569fb6417
pccoder-2000 <chaturvedipriya998@gmail.com>
2021-02-21 17:43:42 +0530

    updated

---
+++ client/src/components/login.component.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import {Row,Col} from 'react-bootstrap';
+
+export default class Login extends Component {
+    render() {
+        return (
+<Row className="landing">
+<Col> <img src ="img/loginimg.png" alt="image"/> </Col>
+<Col>
+            <form>
+                <h3>Sign In</h3>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </form>
+</Col>
+</Row>
+);
+    }
+}
