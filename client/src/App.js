42bbef56e1d2be040cc4df83025422956d361ef0
copyNinja1219 <thapacyrus30@gmail.com>
2021-03-20 17:40:47 +0530

    added files to .gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,47 @@ import {
   Switch,
   BrowserRouter as Routerr,
 } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import Home from './components/home/Home';
+import screenGif from './gif/splash-screen.gif';
 
 function App() {
-  return (
-    <div className='App'>
-      <Router history={history}>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/:error' exact component={Error_} />
-        </Switch>
-      </Router>
-    </div>
-  );
+  const [onLoading, changeLoading] = useState(true);
+  let content = <img src={screenGif} alt='Loading...' />;
+
+  // let content = 'Loading...';
+
+  if (!onLoading)
+    content = (
+      <div className='App'>
+        <Router history={history}>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/:error' exact component={Error_} />
+          </Switch>
+        </Router>
+      </div>
+    );
+
+  useEffect(() => {
+    changeLoading(false);
+  }, []);
+
+  // let contentToShow = (
+  //   <div className='App'>
+  //     <Router history={history}>
+  //       <Switch>
+  //         <Route path='/' exact component={Home} />
+  //         <Route path='/:error' exact component={Error_} />
+  //       </Switch>
+  //     </Router>
+  //   </div>
+  // );
+
+  return content;
+
+  // {onLoading ? { contentToShow } : <div>" loading" </div>}
 }
 
 export default App;
