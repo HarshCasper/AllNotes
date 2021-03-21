1218b2f06f88bee3963b9b3a8c9802c5ff44a03e
copyNinja1219 <thapacyrus30@gmail.com>
2021-03-21 07:31:37 +0530

    added delete route

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 8000;
+const router = express.Router();
+
+// Delete Route
+import DeleteRoute from './Routes/Delete/Delete';
 
 var project = [
-    {
-       "name":"All Notes"
-    }
- ]
- 
+  {
+    name: 'All Notes',
+  },
+];
+
+router.get('/delete/user', DeleteRoute);
 
 app.get('/', (req, res) => {
-    res.json(project)
-  });
-  
-  app.listen(port, () => {
-    console.log(`Application listening on port ${port}!`)
-  });
+  res.json(project);
+});
+
+app.listen(port, () => {
+  console.log(`Application listening on port ${port}!`);
+});
